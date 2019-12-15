a7b30450997f8a6a2e04ebb9c81598b1ca3928ab
Nukk Pointer <nukkpointer@gmail.com>
2019-12-15 17:40:07 +0000

    Code to force app to run until enter is pressed

--- app.py
+++ app.py
@@ -14,3 +14,7 @@ def button_callback(channel):
 #GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 #GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(18,GPIO.FALLING,callback=button_callback)
+
+# Prevent program from ending
+message = input("Press enter to quit\n\n")
+GPIO.cleanup()
